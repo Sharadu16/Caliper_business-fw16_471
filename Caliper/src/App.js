8d56d9d393068c8d4651ce21d0b754337ea4d0bd
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-11-11 13:44:05 +0530

    add

---
+++ Caliper/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MainRoutes from './pages/MainRoutes';
+
+function App() {
+  return (
+    <div className="App">
+       <MainRoutes/>
+    </div>
+  );
+}
+
+export default App;
